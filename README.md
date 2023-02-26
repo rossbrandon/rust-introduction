423f409ecd2f9101e4796351f3a77eb690c824b2
Ross Brandon <ross.brandon@pm.me>
2023-02-26 11:47:25 -0600

    MS learning course complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust Introduction
+
+Exercises from the Microsoft course [Take your first steps with Rust](https://learn.microsoft.com/en-us/training/paths/rust-first-steps/)
---
+++ modules/src/main.rs
@@ -0,0 +1,13 @@
+mod text_processing;
+
+fn count_letters_and_numbers(text: &str) -> (usize, usize) {
+    let number_of_letters = text_processing::letters::count_letters(text);
+    let number_of_numbers = text_processing::numbers::count_numbers(text);
+    (number_of_letters, number_of_numbers)
+}
+
+fn main() {
+    assert_eq!(count_letters_and_numbers("221B Baker Street"), (12, 3));
+    assert_eq!(count_letters_and_numbers("711 Maple Street"), (11, 3));
+    assert_eq!(count_letters_and_numbers("4 Privet Drive"), (11, 1));
+}
